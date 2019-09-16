bad40c73962a62521b7f9a46f2e05b20e7c8cb0b
Jeremy Faivre <contact@jeremyfa.com>
2019-09-17 00:38:17 +0200

    Update compiled files

--- dist/yaml.js
+++ dist/yaml.js
@@ -1,4 +1,4 @@
-(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+(function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 var Dumper, Inline, Utils;
 
 Utils = require('./Utils');
@@ -25,6 +25,9 @@ Dumper = (function() {
       objectEncoder = null;
     }
     output = '';
+    if (typeof input === 'function') {
+      return output;
+    }
     prefix = (indent ? Utils.strRepeat(' ', indent) : '');
     if (inline <= 0 || typeof input !== 'object' || input instanceof Date || Utils.isEmpty(input)) {
       output += prefix + Inline.dump(input, exceptionOnInvalidType, objectEncoder);
@@ -120,6 +123,7 @@ DumpException = (function(superClass) {
   extend(DumpException, superClass);
 
   function DumpException(message, parsedLine, snippet) {
+    DumpException.__super__.constructor.call(this, message);
     this.message = message;
     this.parsedLine = parsedLine;
     this.snippet = snippet;
@@ -149,6 +153,7 @@ ParseException = (function(superClass) {
   extend(ParseException, superClass);
 
   function ParseException(message, parsedLine, snippet) {
+    ParseException.__super__.constructor.call(this, message);
     this.message = message;
     this.parsedLine = parsedLine;
     this.snippet = snippet;
@@ -178,6 +183,7 @@ ParseMore = (function(superClass) {
   extend(ParseMore, superClass);
 
   function ParseMore(message, parsedLine, snippet) {
+    ParseMore.__super__.constructor.call(this, message);
     this.message = message;
     this.parsedLine = parsedLine;
     this.snippet = snippet;
